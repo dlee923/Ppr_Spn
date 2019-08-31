5b7ebd1b2ccdb3a46af3e49ba9e35ada5d7e4267
Daniel Lee <dlee923@gmail.com>
2019-08-31 17:43:52 -0400

    Adding Ingredients Prep TableView

---
+++ Paper Spoon/MealPrepViewController/IngredientsPrepTableView.swift
@@ -0,0 +1,21 @@
+//
+//  IngredientsPrepTableView.swift
+//  Paper Spoon
+//
+//  Created by Daniel Lee on 8/31/19.
+//  Copyright © 2019 DLEE. All rights reserved.
+//
+
+import UIKit
+
+class IngredientsPrepTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
