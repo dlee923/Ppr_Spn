b14d415524beb2d0a73ebe8d588542ca038868d5
Daniel Lee <dlee923@gmail.com>
2019-10-01 22:50:49 -0400

    improved initial loading speed.  still not solved for activity indicator showing before async functions are run

--- Paper Spoon/DashBoardController.swift
+++ Paper Spoon/DashBoardController.swift
@@ -29,19 +29,35 @@ class DashBoardController: UIPageViewController {
         
         // pass brands data to header
         recipeListHeader.brands = self.brands
-        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         // Add activity indicator
-        self.mainThread.async {
-//            self.dispatchGroup.enter()
+        DispatchQueue.main.async {
             self.activityindicator.activityInProgress()
-//            self.dispatchGroup.leave()
         }
         
         self.downloadData()
     }
+
     
-    override func viewDidAppear(_ animated: Bool) {
+    fileprivate func downloadData() {
+        // download recipe options
+        self.retrieveHelloFreshMenu()
         
+        // download recipes after downloading menu
+        self.retrieveRecipeData()
+        
+        // download thumbnail after retrieving recipe data
+        self.retrieveThumbnail()
+        
+        self.dispatchGroup.notify(queue: self.mainThread) {
+            // update UI
+            print("Updating UI")
+            self.recipeListViewController.menuOptionList.reloadData()
+            // Stop activity indicator
+            self.activityindicator.activityEnded()
+        }
     }
     
     lazy var recipeListHeader = RecipeListHeader(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 100))
@@ -67,7 +83,7 @@ class DashBoardController: UIPageViewController {
     
     let activityindicator = ActivityIndicator()
     let dispatchGroup = DispatchGroup()
-    let backgroundThread = DispatchQueue.global(qos: .background)
+    let backgroundThread = DispatchQueue(label: "backgroundThread")
     let mainThread = DispatchQueue.main
     var brands: [Brand]?
     
@@ -78,26 +94,6 @@ class DashBoardController: UIPageViewController {
         
         if let recipeListVC1 = controllers.first {
             self.setViewControllers([recipeListVC1], direction: .forward, animated: false, completion: nil)
-        }
-    }
-    
-    
-    fileprivate func downloadData() {
-        // download recipe options
-        self.retrieveHelloFreshMenu()
-        
-        // download recipes after downloading menu
-        self.retrieveRecipeData()
-        
-        // download thumbnail after retrieving recipe data
-        self.retrieveThumbnail()
-        
-        self.dispatchGroup.notify(queue: self.mainThread) {
-            // update UI
-            print("Updating UI")
-            self.recipeListViewController.menuOptionList.reloadData()
-            // Stop activity indicator
-            self.activityindicator.activityEnded()
         }
     }
     
@@ -113,6 +109,7 @@ class DashBoardController: UIPageViewController {
             })
         }
         self.dispatchGroup.enter()
+        
         backgroundThread.async(group: dispatchGroup, execute: retrieveMenuOptions)
         self.dispatchGroup.wait()
     }
@@ -120,7 +117,7 @@ class DashBoardController: UIPageViewController {
     
     fileprivate func retrieveRecipeData() {
         guard let menuOptions = self.recipeListViewController.menuOptionsObj.menuOptions else { return }
-        
+        var menuOptionsCount = 0
         // download recipe details for each menu option
         for menuOption in menuOptions {
             let retrieveRecipeData = DispatchWorkItem {
@@ -129,15 +126,25 @@ class DashBoardController: UIPageViewController {
                     // find index for recipe in menu options and attach recipe object
                     if let menuIndex = self.recipeListViewController.menuOptionsObj.menuOptions?.firstIndex(where: { $0.recipeLink == menuOption.recipeLink }) {
                         self.recipeListViewController.menuOptionsObj.menuOptions?[menuIndex].recipe = recipe
-                        self.dispatchGroup.leave()
+                        
+                        // increment count through menu options
+                        menuOptionsCount += 1
+                        print("\(menuOptionsCount) / \(menuOptions.count)")
+                        
+                        // trigger leave group if final completion
+                        if menuOptionsCount >= menuOptions.count {
+                            print("leave group")
+                            self.dispatchGroup.leave()
+                        }
                     }
                 })
             }
-            self.dispatchGroup.enter()
             self.backgroundThread.async(group: dispatchGroup, execute: retrieveRecipeData)
-            self.dispatchGroup.wait()
         }
+        self.dispatchGroup.enter()
+        self.dispatchGroup.wait()
     }
+    
     
     fileprivate func retrieveThumbnail() {
         guard let menuOptions = self.recipeListViewController.menuOptionsObj.menuOptions else { return }
@@ -150,6 +157,7 @@ class DashBoardController: UIPageViewController {
                     ImageAPI.shared.downloadImage(urlLink: thumbnailLink, completion: { (thumbnailData) in
                         if let menuIndex = self.recipeListViewController.menuOptionsObj.menuOptions?.firstIndex(where: { $0.recipeLink == menuOption.recipeLink }) {
                             self.recipeListViewController.menuOptionsObj.menuOptions?[menuIndex].recipe?.thumbnail = UIImage(data: thumbnailData)
+                            print("image downloaded")
                             self.dispatchGroup.leave()
                         }
                     })
@@ -167,6 +175,7 @@ class DashBoardController: UIPageViewController {
         */
     }
     
+    
     private func addRecipeListHeader() {
         self.view.addSubview(self.recipeListHeader)
         self.recipeListHeader.translatesAutoresizingMaskIntoConstraints = false
@@ -177,6 +186,7 @@ class DashBoardController: UIPageViewController {
             self.recipeListHeader.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 5)
         ])
     }
+    
     
     private func createBrands() {
         let helloFresh = Brand(name: .HelloFresh, image: UIImage(named: "hellofresh_x1.png")!)
