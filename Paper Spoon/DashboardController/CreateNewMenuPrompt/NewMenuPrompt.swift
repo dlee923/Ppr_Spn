8c0990d1af13019813139aa4b4deadb96406de3e
Daniel Lee <dlee923@gmail.com>
2019-11-03 07:38:41 -0500

    Edit menu label when editing textfield

--- Paper Spoon/DashboardController/CreateNewMenuPrompt/NewMenuPrompt.swift
+++ Paper Spoon/DashboardController/CreateNewMenuPrompt/NewMenuPrompt.swift
@@ -16,7 +16,7 @@ class NewMenuPrompt: UIView {
     }
     
     private func setup() {
-        self.backgroundColor = .red
+        self.backgroundColor = .yellow
         self.addMenuTitle()
         self.addMenuNameField()
         self.addCreateBtn()
@@ -30,17 +30,19 @@ class NewMenuPrompt: UIView {
     let createBtn = UIButton()
     
     private func addMenuTitle() {
-        self.menuTitle.font = UIFont.fontSunflower?.withSize(20)
+        self.menuTitle.font = UIFont.fontSunflower?.withSize(50)
         self.menuTitle.textColor = UIColor.color2
+        self.menuTitle.connect(with: self.menuNameField)
+        self.menuTitle
         
         self.addSubview(self.menuTitle)
         self.menuTitle.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             self.menuTitle.topAnchor.constraint(equalTo: self.topAnchor),
-            self.menuTitle.leadingAnchor.constraint(equalTo: self.leadingAnchor),
-            self.menuTitle.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            self.menuTitle.bottomAnchor.constraint(equalTo: self.centerYAnchor)
+            self.menuTitle.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5),
+            self.menuTitle.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -5)
         ])
+        self.menuTitle.sizeToFit()
     }
     
     private func addMenuNameField() {
@@ -48,7 +50,7 @@ class NewMenuPrompt: UIView {
         self.menuNameField.placeholder = "Enter name for menu"
         self.menuNameField.font = UIFont.fontBebas?.withSize(20)
         self.menuNameField.textAlignment = .center
-        self.menuNameField.backgroundColor = UIColor.white.withAlphaComponent(0.3)
+        self.menuNameField.backgroundColor = UIColor.black.withAlphaComponent(0.3)
         
         self.menuNameField.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
@@ -67,7 +69,7 @@ class NewMenuPrompt: UIView {
         self.createBtn.setTitle("Submit", for: .normal)
         self.createBtn.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            self.createBtn.topAnchor.constraint(equalTo: self.menuNameField.bottomAnchor),
+            self.createBtn.topAnchor.constraint(equalTo: self.menuNameField.bottomAnchor, constant: 10),
             self.createBtn.leadingAnchor.constraint(equalTo: self.menuNameField.leadingAnchor),
             self.createBtn.trailingAnchor.constraint(equalTo: self.menuNameField.trailingAnchor, constant: -70),
             self.createBtn.heightAnchor.constraint(equalToConstant: 40)
@@ -77,11 +79,12 @@ class NewMenuPrompt: UIView {
     private func addNoThanksBtn() {
         self.addSubview(self.noThanksBtn)
         self.noThanksBtn.titleLabel?.font = UIFont.fontCoolvetica?.withSize(10)
-        self.noThanksBtn.titleLabel?.textAlignment = .right
+        self.noThanksBtn.setTitleColor(.themeColor1, for: .normal)
+        self.noThanksBtn.contentHorizontalAlignment = .right
         self.noThanksBtn.setTitle("No Thanks", for: .normal)
         self.noThanksBtn.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            self.noThanksBtn.topAnchor.constraint(equalTo: self.menuNameField.bottomAnchor),
+            self.noThanksBtn.topAnchor.constraint(equalTo: self.menuNameField.bottomAnchor, constant: 10),
             self.noThanksBtn.leadingAnchor.constraint(equalTo: self.createBtn.trailingAnchor, constant: 5),
             self.noThanksBtn.trailingAnchor.constraint(equalTo: self.menuNameField.trailingAnchor),
             self.noThanksBtn.heightAnchor.constraint(equalToConstant: 40)
@@ -94,8 +97,13 @@ class NewMenuPrompt: UIView {
    
 }
 
-extension NewMenuPrompt: UITextFieldDelegate {
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        <#code#>
+
+extension UILabel {
+    @objc func input(textField: UITextField) {
+        self.text = textField.text
+    }
+    
+    func connect(with textField: UITextField) {
+        textField.addTarget(self, action: #selector(input(textField:)), for: .editingChanged)
     }
 }
